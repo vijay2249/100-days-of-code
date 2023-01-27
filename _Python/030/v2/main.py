d24c820899b935d391a342ac9e1eb4b3a1b1b466
VIJAY 'H'CN <65058877+vijay2249@users.noreply.github.com>
2023-01-27 12:52:23 +0530

    dummy data + writing data into json

---
+++ _Python/030/v2/main.py
@@ -0,0 +1,128 @@
+try:
+    from tkinter import *
+    from tkinter import messagebox
+    from random import randint, sample, shuffle
+    import json
+    import pyperclip
+except ModuleNotFoundError:
+    import subprocess
+    subprocess.call(['pip', 'install', 'pyperclip'], shell=True)
+    import pyperclip
+
+# -------------- Error Classes ----------------------
+class ResultNotFound(Exception):
+    errorName = "Result(s) Not Found"
+    def __init__(self, data, *args):
+        super().__init__(args)
+        self.website = data
+    
+    def __str__(self) -> str:
+        return f"login details for \"{self.website}\" wesbite were not found"
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = sample(letters, randint(8, 10))
+    password_list += sample(numbers, randint(2, 4))
+    password_list += sample(symbols, randint(2, 4))
+    shuffle(password_list)
+    password = "".join(password_list)
+    passwordEntry.delete(0,END)
+    passwordEntry.insert(0, password)
+    pyperclip.copy(password)
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website, email, password = websiteEntry.get(), emailEntry.get(), passwordEntry.get()
+    new_data = {website :{'email': email, 'password': password}}
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title='Wrong Input', message='No fields should be left empty')
+    else:
+        msg = f"These are the details entered:\n\nEmail: {email}\nPassword: {password}\nIs it okay to save..."
+        isOk = messagebox.askokcancel(title=website, message=msg)
+        if isOk:
+            try:
+                with open('data.json', 'r') as f:
+                    data = json.load(f)
+            except FileNotFoundError:
+                with open('data.json', 'w') as f:
+                    json.dump(new_data, f, indent=2)
+            else:
+                data.update(new_data)
+                with open('data.json', 'w') as f:
+                    json.dump(data, f, indent=2)
+            finally:
+                websiteEntry.delete(0,END)
+                # emailEntry.delete(0,END)
+                passwordEntry.delete(0,END)
+
+def searchPass(website):
+    try:
+        assert len(website) != 0
+        with open('data.json', 'r') as f:
+            data = json.load(f)
+            if website in data.keys():
+                email = data[website]['email']
+                password = data[website]['password']
+                msg = f"Details found:\n\nWebsite: {website}\nPassword: {password}\nEmail: {email}\nClick Ok to copy the password to clipboard"
+                isOk = messagebox.askokcancel(title=website, message=msg)
+                if isOk:
+                    pyperclip.copy(password)
+            else:
+                raise ResultNotFound(website)
+    except AssertionError as err:
+        pass
+    except FileNotFoundError as err:
+        messagebox.showerror(title="File Not found", message="No passwords saved file exists")
+    except ResultNotFound as err:
+        messagebox.showerror(title=err.errorName, message=err)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file='logo.png')
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0,column=1)
+
+#  --------------- Labels --------------- 
+websiteLabel = Label(text='Website:')
+websiteLabel.focus()
+websiteLabel.grid(row=1, column=0)
+email_user_label = Label(text='Email/Username:')
+email_user_label.grid(row=2,column=0)
+passwordLabel = Label(text='Password:')
+passwordLabel.grid(row=3,column=0)
+
+# --------------- entries -------------
+websiteEntry = Entry(width=30)
+websiteEntry.grid(row=1, column=1)
+emailEntry = Entry(width=40)
+emailEntry.grid(row=2, column=1, columnspan=2)
+emailEntry.insert(0, 'email_id')
+passwordEntry = Entry(width=30)
+passwordEntry.grid(row=3, column=1)
+
+# ----------- BUTTONS -------------------
+generatePass = Button(text="Generate", command=generate_password)
+generatePass.grid(row=3, column=2)
+add = Button(text='Add content', width=35, command=save)
+add.grid(row=4, column=1, columnspan=2)
+# searchOption = Button(text='Search', command=searchPass)
+searchOption = Button(text='Search', command=lambda: searchPass(websiteEntry.get()))
+searchOption.grid(row=1,column=2)
+
+window.mainloop()
+
+
+
+# what if there are multiple accounts on same website
+# error handling for that if not allowed(ideally should be allowed)
+# how to access the login data for this type of multiple 
